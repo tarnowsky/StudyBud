c26aaccb4510b4909e957bab46e74e285fe53435
Michal-Tarnowski <michal.tarnowski03@gmail.com>
2023-02-10 14:02:00 +0100

    Profile updated

--- base/views.py
+++ base/views.py
@@ -99,7 +99,9 @@ def room(request, pk):
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
     rooms = user.room_set.all()
-    context = {'user': user, 'rooms': rooms}
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics' : topics}
     return render(request, 'base/profile.html', context)
 
 @login_required(login_url='/login')

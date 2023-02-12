604353f3db69958256cd1664932613f4a9c913f3
Michal-Tarnowski <michal.tarnowski03@gmail.com>
2023-02-12 13:16:49 +0100

    Activities page added

--- base/views.py
+++ base/views.py
@@ -72,7 +72,7 @@ def home(request):
         Q(description__icontains=q)
     )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -182,5 +182,12 @@ def updateUser(request):
     return render(request, 'base/update-user.html', context)
 
 def topicPage(request):
-    context = {}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics': topics}
     return render(request, 'base/topics.html', context)
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages': room_messages}
+    return render(request, 'base/activity.html', context)

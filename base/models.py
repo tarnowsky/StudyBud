204f19d249a936ace37683c59d3dc39acfcf6b00
Michal-Tarnowski <michal.tarnowski03@gmail.com>
2023-02-13 00:27:14 +0100

    Starting to Add new AbstractUser Model

--- base/models.py
+++ base/models.py
@@ -1,39 +1,42 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
 
-class Topic(models.Model):
-    name = models.CharField(max_length=200)
+# from django.contrib.auth.models import User
 
-    def __str__(self):
-        return self.name
+# class Topic(models.Model):
+#     name = models.CharField(max_length=200)
 
-class Room(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    participants = models.ManyToManyField(User, related_name='participants', blank=True)
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
+#     def __str__(self):
+#         return self.name
 
-    class Meta:
-        ordering = ['-updated', '-created']
+# class Room(models.Model):
+#     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+#     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+#     name = models.CharField(max_length=200)
+#     description = models.TextField(null=True, blank=True)
+#     participants = models.ManyToManyField(User, related_name='participants', blank=True)
+#     updated = models.DateTimeField(auto_now=True)
+#     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
+#     class Meta:
+#         ordering = ['-updated', '-created']
 
-class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    body = models.TextField()
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
+#     def __str__(self):
+#         return self.name
 
-    class Meta:
-        ordering = ['-updated', '-created']
+# class Message(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     room = models.ForeignKey(Room, on_delete=models.CASCADE)
+#     body = models.TextField()
+#     updated = models.DateTimeField(auto_now=True)
+#     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.body[0:50]
+#     class Meta:
+#         ordering = ['-updated', '-created']
+
+#     def __str__(self):
+#         return self.body[0:50]
 

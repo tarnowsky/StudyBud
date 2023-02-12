83b8947cca8bbfacafd69b91a81e8f515b0d5c4c
Michal-Tarnowski <michal.tarnowski03@gmail.com>
2023-02-12 22:43:49 +0100

    Updated views and created serializers for Room query objects

--- base/api/views.py
+++ base/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
+from .serializers import RoomSerializer
 
 
 @api_view(['GET'])
@@ -15,4 +16,11 @@ def getRoutes(request):
 @api_view(['GET'])
 def getRooms(request):
     rooms = Room.objects.all()
-    return Response(rooms)
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    rooms = Room.objects.get(id=pk)
+    serializer = RoomSerializer(rooms, many=False)
+    return Response(serializer.data)

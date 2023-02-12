0bb1d7eac90ec293ebe3833d04a300a462c1281d
Michal-Tarnowski <michal.tarnowski03@gmail.com>
2023-02-13 00:45:08 +0100

    Updated models to be able to login by email

--- base/models.py
+++ base/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)

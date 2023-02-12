41b6d8092130feb76c842b2d3c6e00c4dd689732
Michal-Tarnowski <michal.tarnowski03@gmail.com>
2023-02-12 21:55:27 +0100

    Files for API to work created

---
+++ base/api/views.py
@@ -0,0 +1,18 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'GET /api',
+        'GET /api/rooms',
+        'GET /api/rooms/:id',
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    return Response(rooms)

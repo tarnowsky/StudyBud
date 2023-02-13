87416f78dac6ea35ac5cc64f109a25d9a140b359
Michal-Tarnowski <michal.tarnowski03@gmail.com>
2023-02-13 02:02:43 +0100

    Profile pictures and login by email created

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,17 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, User
 
+class MuUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            'name', 
+            'username', 
+            'email', 
+            'password1', 
+            'password2',
+        ]
 
 class RoomForm(ModelForm):
     class Meta:
@@ -11,4 +22,4 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email',]
+        fields = ['avatar', 'name', 'username', 'email', 'bio',]
--- base/views.py
+++ base/views.py
@@ -4,9 +4,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MuUserCreationForm
 
 # rooms = [
 #     {'id': 1, 'name': 'Lets learn python!'},
@@ -20,18 +19,18 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
         userInDB = False
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
             userInDB = True
         except:
             messages.error(request, 'User does not exist')
 
         if userInDB:
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(request, email=email, password=password)
 
             if user is not None:
                 login(request, user)
@@ -48,9 +47,9 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MuUserCreationForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MuUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -173,7 +172,7 @@ def updateUser(request):
     user = request.user
     form = UserForm(instance=user)
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile', pk=user.id)
